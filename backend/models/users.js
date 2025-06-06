45847b27c75fa9400b7a08ab87ae6e213594f49f
palavalasa29 <palavalasabhargavnaidu@gmail.com>
2025-06-06 18:04:28 +0530

    pushin user model

---
+++ backend/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true });
+
+const Users = mongoose.model("Users", userSchema);
+
+module.exports = Users;
